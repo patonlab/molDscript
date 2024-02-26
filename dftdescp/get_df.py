c203c1f1ee2a9370b0b6695763000f9ecf326218
Jake King <97928856+j77king@users.noreply.github.com>
2024-02-25 22:29:27 -0700

    get_df file

---
+++ dftdescp/get_df.py
@@ -0,0 +1,35 @@
+######################################################.
+#        This file stores the get_df class            #
+######################################################.
+
+
+import pandas as pd
+from dftdescp.argument_parser import command_line_args
+
+
+class get_df:
+    """
+    Class to create a dataframe of parameters.
+    """
+
+    def __init__(self, data_dicts):
+        self.df = False
+        for dict in data_dicts:
+            dict_df = False
+            for file_name in dict.keys():
+                value_column = []
+                property_column = []
+                for property in dict[file_name].keys():
+                    value = dict[file_name][property]
+                    value_column.append(value)
+                    property_column.append(property)
+                if dict_df == False:
+                    data = {"Property": property_column, file_name: value_column}
+                    dict_df = pd.DataFrame(data)
+                else:
+                    dict_df[file_name] = value_column
+            if self.df == False:
+                self.df = dict_df
+            else:
+                pd.concat(self.df, dict_df)
+        return self.df
