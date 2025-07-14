c5f98fb013101e9d956ae291b7460f480886836b
Jacob King <jake.king@colostate.edu>
2025-07-14 11:58:56 -0600

    program will not fail if no bond indexes in NBO

--- moldscript/nbo.py
+++ moldscript/nbo.py
@@ -62,7 +62,8 @@ class nbo:
                 file_name = get_filename(file_name, self.data_dict)
                 self.data_dict[file_name]['atom']["natural_charge"] = nbo_data.atomcharges["natural"]
                 self.data_dict[file_name]['atom']["bond_orders"] = nbo_data.bondorders
-                self.data_dict[file_name]['bond']["bond_order_matrix"] = nbo_data.bondorders_matrix
+                if nbo_data.bondorders_matrix != []:
+                    self.data_dict[file_name]['bond']["bond_order_matrix"] = nbo_data.bondorders_matrix
 
                 
             else:
